e24a1eb696451b28a838aff92641199346429953
Haider Ali <haider.ali07304@gmail.com>
2024-01-25 19:26:20 -0500

    Fixed ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# CreditCardFraud
+# Credit Card Fraud Detection
+
+## Project Overview
+This project applies machine learning techniques to identify fraudulent transactions from credit card data. It aims to provide a robust solution that can be integrated into financial systems for real-time fraud prevention.
+
+## Dataset Description
+The dataset contains transactions made by credit cards, where each transaction is labeled as fraudulent or non-fraudulent. Due to confidentiality, the features are transformed using PCA, with 'Amount' and 'Class' as the only original features.
+
+## Machine Learning Models
+Several models have been trained and evaluated:
+- Decision Tree Classifier
+- K-Nearest Neighbors (KNN)
+- Logistic Regression
+- Support Vector Machine (SVM)
+- Random Forest Classifier
+- XGBoost Classifier
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+What you need to install the software:
+- Python 3.x
+- Pandas
+- NumPy
+- Matplotlib
+- Scikit-learn
+- XGBoost
+
--- creditCardFraud.py
+++ creditCardFraud.py
@@ -1,234 +1,99 @@
 # IMPORTING PACKAGES
 
-import pandas as pd # data processing
-import numpy as np # working with arrays
-import matplotlib.pyplot as plt # visualization
-from termcolor import colored as cl # text customization
-import itertools # advanced tools
-
-from sklearn.preprocessing import StandardScaler # data normalization
-from sklearn.model_selection import train_test_split # data split
-from sklearn.tree import DecisionTreeClassifier # Decision tree algorithm
-from sklearn.neighbors import KNeighborsClassifier # KNN algorithm
-from sklearn.linear_model import LogisticRegression # Logistic regression algorithm
-from sklearn.svm import SVC # SVM algorithm
-from sklearn.ensemble import RandomForestClassifier # Random forest tree algorithm
-from xgboost import XGBClassifier # XGBoost algorithm
-
-from sklearn.metrics import confusion_matrix # evaluation metric
-from sklearn.metrics import accuracy_score # evaluation metric
-from sklearn.metrics import f1_score # evaluation metric
+import itertools
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 
 # IMPORTING DATA
-
-df = pd.read_csv('creditcard.csv')
-df.drop('Time', axis = 1, inplace = True)
-
-print(df.head())
+credit_data = pd.read_csv('uploads/creditcard.csv')
+credit_data.drop('Time', axis=1, inplace=True)
 
 # EDA
-
-# 1. Count & percentage
-
-cases = len(df)
-nonfraud_count = len(df[df.Class == 0])
-fraud_count = len(df[df.Class == 1])
-fraud_percentage = round(fraud_count/nonfraud_count*100, 2)
-
-print(cl('CASE COUNT', attrs = ['bold']))
-print(cl('--------------------------------------------', attrs = ['bold']))
-print(cl('Total number of cases are {}'.format(cases), attrs = ['bold']))
-print(cl('Number of Non-fraud cases are {}'.format(nonfraud_count), attrs = ['bold']))
-print(cl('Number of Non-fraud cases are {}'.format(fraud_count), attrs = ['bold']))
-print(cl('Percentage of fraud cases is {}'.format(fraud_percentage), attrs = ['bold']))
-print(cl('--------------------------------------------', attrs = ['bold']))
-
-# 2. Description
-
-nonfraud_cases = df[df.Class == 0]
-fraud_cases = df[df.Class == 1]
-
-print(cl('CASE AMOUNT STATISTICS', attrs = ['bold']))
-print(cl('--------------------------------------------', attrs = ['bold']))
-print(cl('NON-FRAUD CASE AMOUNT STATS', attrs = ['bold']))
-print(nonfraud_cases.Amount.describe())
-print(cl('--------------------------------------------', attrs = ['bold']))
-print(cl('FRAUD CASE AMOUNT STATS', attrs = ['bold']))
-print(fraud_cases.Amount.describe())
-print(cl('--------------------------------------------', attrs = ['bold']))
+total_transactions = len(credit_data)
+legit_transactions = len(credit_data[credit_data.Class == 0])
+fraud_transactions = len(credit_data[credit_data.Class == 1])
+fraud_ratio = round(fraud_transactions / legit_transactions * 100, 2)
 
 # DATA SPLIT
+features = credit_data.drop('Class', axis=1).values
+labels = credit_data['Class'].values
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=0)
 
-X = df.drop('Class', axis = 1).values
-y = df['Class'].values
+# MODELS
+decision_tree = DecisionTreeClassifier(max_depth=4, criterion='entropy')
+k_nearest_neighbors = KNeighborsClassifier(n_neighbors=5)
+logistic_reg = LogisticRegression()
+support_vector_machine = SVC()
+random_forest = RandomForestClassifier(max_depth=4)
+extreme_gradient_boosting = XGBClassifier(max_depth=4)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# TRAINING
+decision_tree.fit(features_train, labels_train)
+k_nearest_neighbors.fit(features_train, labels_train)
+logistic_reg.fit(features_train, labels_train)
+support_vector_machine.fit(features_train, labels_train)
+random_forest.fit(features_train, labels_train)
+extreme_gradient_boosting.fit(features_train, labels_train)
 
-print(cl('X_train samples : ', attrs = ['bold']), X_train[:1])
-print(cl('X_test samples : ', attrs = ['bold']), X_test[0:1])
-print(cl('y_train samples : ', attrs = ['bold']), y_train[0:10])
-print(cl('y_test samples : ', attrs = ['bold']), y_test[0:10])
-
-# MODELING
-
-# 1. Decision Tree
-
-tree_model = DecisionTreeClassifier(max_depth = 4, criterion = 'entropy')
-tree_model.fit(X_train, y_train)
-tree_yhat = tree_model.predict(X_test)
-
-# 2. K-Nearest Neighbors
-
-n = 5
-
-knn = KNeighborsClassifier(n_neighbors = n)
-knn.fit(X_train, y_train)
-knn_yhat = knn.predict(X_test)
-
-# 3. Logistic Regression
-
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-lr_yhat = lr.predict(X_test)
-
-# 4. SVM 
-
-svm = SVC()
-svm.fit(X_train, y_train)
-svm_yhat = svm.predict(X_test)
-
-# 5. Random Forest Tree
-
-rf = RandomForestClassifier(max_depth = 4)
-rf.fit(X_train, y_train)
-rf_yhat = rf.predict(X_test)
-
-# 6. XGBoost
-
-xgb = XGBClassifier(max_depth = 4)
-xgb.fit(X_train, y_train)
-xgb_yhat = xgb.predict(X_test)
+# PREDICTIONS
+decision_tree_predictions = decision_tree.predict(features_test)
+knn_predictions = k_nearest_neighbors.predict(features_test)
+logistic_reg_predictions = logistic_reg.predict(features_test)
+svm_predictions = support_vector_machine.predict(features_test)
+random_forest_predictions = random_forest.predict(features_test)
+xgboost_predictions = extreme_gradient_boosting.predict(features_test)
 
 # EVALUATION
+print(f'Accuracy of Decision Tree: {accuracy_score(labels_test, decision_tree_predictions)}')
+print(f'Accuracy of KNN: {accuracy_score(labels_test, knn_predictions)}')
+print(f'Accuracy of Logistic Regression: {accuracy_score(labels_test, logistic_reg_predictions)}')
+print(f'Accuracy of SVM: {accuracy_score(labels_test, svm_predictions)}')
+print(f'Accuracy of Random Forest: {accuracy_score(labels_test, random_forest_predictions)}')
+print(f'Accuracy of XGBoost: {accuracy_score(labels_test, xgboost_predictions)}')
 
-# 1. Accuracy score
+print(f'F1 Score of Decision Tree: {f1_score(labels_test, decision_tree_predictions)}')
+print(f'F1 Score of KNN: {f1_score(labels_test, knn_predictions)}')
+print(f'F1 Score of Logistic Regression: {f1_score(labels_test, logistic_reg_predictions)}')
+print(f'F1 Score of SVM: {f1_score(labels_test, svm_predictions)}')
+print(f'F1 Score of Random Forest: {f1_score(labels_test, random_forest_predictions)}')
+print(f'F1 Score of XGBoost: {f1_score(labels_test, xgboost_predictions)}')
 
-print(cl('ACCURACY SCORE', attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the Decision Tree model is {}'.format(accuracy_score(y_test, tree_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the KNN model is {}'.format(accuracy_score(y_test, knn_yhat)), attrs = ['bold'], color = 'green'))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the Logistic Regression model is {}'.format(accuracy_score(y_test, lr_yhat)), attrs = ['bold'], color = 'red'))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the SVM model is {}'.format(accuracy_score(y_test, svm_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the Random Forest Tree model is {}'.format(accuracy_score(y_test, rf_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('Accuracy score of the XGBoost model is {}'.format(accuracy_score(y_test, xgb_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-
-# 2. F1 score
-
-print(cl('F1 SCORE', attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the Decision Tree model is {}'.format(f1_score(y_test, tree_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the KNN model is {}'.format(f1_score(y_test, knn_yhat)), attrs = ['bold'], color = 'green'))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the Logistic Regression model is {}'.format(f1_score(y_test, lr_yhat)), attrs = ['bold'], color = 'red'))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the SVM model is {}'.format(f1_score(y_test, svm_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the Random Forest Tree model is {}'.format(f1_score(y_test, rf_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-print(cl('F1 score of the XGBoost model is {}'.format(f1_score(y_test, xgb_yhat)), attrs = ['bold']))
-print(cl('------------------------------------------------------------------------', attrs = ['bold']))
-
-# 3. Confusion Matrix
-
-# defining the plot function
-
-def plot_confusion_matrix(cm, classes, title, normalize = False, cmap = plt.cm.Blues):
-    title = 'Confusion Matrix of {}'.format(title)
-    if normalize:
-        cm = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
-
-    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+# CONFUSION MATRIX PLOTTING
+def plot_confusion_matrix(cm, classes, title):
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation = 45)
+    plt.xticks(tick_marks, classes)
     plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment = 'center',
-                 color = 'white' if cm[i, j] > thresh else 'black')
-
-    plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
-# Compute confusion matrix for the models
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
 
-tree_matrix = confusion_matrix(y_test, tree_yhat, labels = [0, 1]) # Decision Tree
-knn_matrix = confusion_matrix(y_test, knn_yhat, labels = [0, 1]) # K-Nearest Neighbors
-lr_matrix = confusion_matrix(y_test, lr_yhat, labels = [0, 1]) # Logistic Regression
-svm_matrix = confusion_matrix(y_test, svm_yhat, labels = [0, 1]) # Support Vector Machine
-rf_matrix = confusion_matrix(y_test, rf_yhat, labels = [0, 1]) # Random Forest Tree
-xgb_matrix = confusion_matrix(y_test, xgb_yhat, labels = [0, 1]) # XGBoost
-
-# Plot the confusion matrix
-
-plt.rcParams['figure.figsize'] = (6, 6)
-
-# 1. Decision tree
-
-tree_cm_plot = plot_confusion_matrix(tree_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'Decision Tree')
-plt.savefig('tree_cm_plot.png')
-plt.show()
-
-# 2. K-Nearest Neighbors
-
-knn_cm_plot = plot_confusion_matrix(knn_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'KNN')
-plt.savefig('knn_cm_plot.png')
-plt.show()
-
-# 3. Logistic regression
-
-lr_cm_plot = plot_confusion_matrix(lr_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'Logistic Regression')
-plt.savefig('lr_cm_plot.png')
-plt.show()
-
-# 4. Support Vector Machine
-
-svm_cm_plot = plot_confusion_matrix(svm_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'SVM')
-plt.savefig('svm_cm_plot.png')
-plt.show()
-
-# 5. Random forest tree
-
-rf_cm_plot = plot_confusion_matrix(rf_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'Random Forest Tree')
-plt.savefig('rf_cm_plot.png')
-plt.show()
-
-# 6. XGBoost
-
-xgb_cm_plot = plot_confusion_matrix(xgb_matrix, 
-                                classes = ['Non-Default(0)','Default(1)'], 
-                                normalize = False, title = 'XGBoost')
-plt.savefig('xgb_cm_plot.png')
-plt.show()
+# CONFUSION MATRICES FOR EACH MODEL
+for model, predictions, title in zip(
+    [decision_tree, k_nearest_neighbors, logistic_reg, support_vector_machine, random_forest, extreme_gradient_boosting],
+    [decision_tree_predictions, knn_predictions, logistic_reg_predictions, svm_predictions, random_forest_predictions, xgboost_predictions],
+    ['Decision Tree', 'KNN', 'Logistic Regression', 'SVM', 'Random Forest', 'XGBoost']
+):
+    conf_matrix = confusion_matrix(labels_test, predictions)
+    plt.figure()
+    plot_confusion_matrix(conf_matrix, classes=['Legit(0)', 'Fraud(1)'], title=f'Confusion Matrix of {title}')
+    plt.savefig(f'{title}_confusion_matrix.png')
+    plt.show()
